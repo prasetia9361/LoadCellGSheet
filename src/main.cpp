01694c480e928a5c8a0bfd9bb2ee831057dfa0e5
prasetia9361 <prasetia1805@gmail.com>
2025-08-09 16:21:41 +0700

    updateServo

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,7 @@ Servo servoYaw;
 Servo servoPitch;
 
 // Kalibrasi timbangan
-const float CALIBRATION_FACTOR = 189.00;//60.88
+const float CALIBRATION_FACTOR = 189.00;
 float offset = 0;
 
 // Debounce button
@@ -27,6 +27,7 @@ const unsigned long DEBOUNCE_DELAY = 50;
 
 // Status gerakan servo
 bool isServoMoving = false;
+bool lastButtonState = HIGH;
 
 // WiFi credentials
 const char* ssid     = "wefee"; 
@@ -37,16 +38,19 @@ const float BERAT_C = 100.0;
 const float BERAT_B = 200.0;
 const float BERAT_A = 300.0;
 
+void applicationTask0(void *param);
+void applicationTask1(void *param);
+
 float berat;
-String kategori;
+String kategori = "stay";
 
-enum State {
-  IDLE,
-  MEASURING,
-  MOVING
-};
+// enum State {
+//   IDLE,
+//   MEASURING,
+//   MOVING
+// };
 
-State currentState = IDLE;
+// State currentState = IDLE;
 unsigned long lastMoveTime = 0;
 
 
@@ -54,27 +58,45 @@ float getWeight() {
   // if (!scale.wait_ready_timeout(200)) return -1.0;
   // return abs(scale.get_units(10));
   long sum = 0;
-  for (int i = 0; i < 100; i++) {
+  for (int i = 0; i < 120; i++) {
       while (!scale.is_ready()) {};        // tunggu siap
       sum += scale.get_units();           // baca bobot
   }
-  float beban = sum / 100.0;               // rata-rata 10 bacaan
+  float beban = sum / 120.0;               // rata-rata 10 bacaan
   return beban;
 }
 
 void tareScale() {
   Serial.println("Proses tare...");
-  scale.tare(100);
+  scale.tare(120);
   offset = scale.get_offset();
-  Serial.println("Offset baru: " + String(offset));
+  // Serial.println("Offset baru: " + String(offset));
 }
 
 void checkTareButton() {
-  if (digitalRead(BUTTON_PIN) == LOW && (millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-    tareScale();
-    Serial.println("Tare berhasil!");
-    lastDebounceTime = millis();
+  // static int lastButtonState = HIGH;
+  // int currentButtonState = digitalRead(BUTTON_PIN);
+
+  // if (lastButtonState == HIGH && currentButtonState == LOW && (millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
+  //   tareScale();
+  //   Serial.println("Tare berhasil!");
+  //   lastDebounceTime = millis();
+  // }
+  // lastButtonState = currentButtonState;
+
+  bool currentButtonState = digitalRead(BUTTON_PIN);
+
+  if (currentButtonState != lastButtonState)
+  {
+    if (currentButtonState == LOW)
+    {
+      tareScale();
+      Serial.println("Tare berhasil!");
+      lastDebounceTime = millis();
+    }
+    lastButtonState = currentButtonState;
   }
+
 }
 
 String tentukanKategori(float w) {
@@ -95,7 +117,6 @@ void moveServo(Servo &sv, int startDeg, int endDeg, int stepDeg) {
   int step = abs(stepDeg) * dir;
   for (int pos = startDeg; (dir > 0 ? pos <= endDeg : pos >= endDeg); pos += step) {
     sv.write(pos);
-    // isServoMoving = true;
     delay(100);
   }
 }
@@ -147,45 +168,132 @@ void setup() {
   // tareScale();
 
   // Koneksi WiFi
-  Sheet.connectWiFi(ssid, password);
+  TaskHandle_t task0;
+  xTaskCreatePinnedToCore(
+    applicationTask0,
+    "applicationTask0",
+    10000,
+    NULL,
+    1,
+    &task0,
+    0
+  );
+
+  TaskHandle_t task1;
+  xTaskCreatePinnedToCore(
+    applicationTask1,
+    "applicationTask1",
+    10000,
+    NULL,
+    1,
+    &task0,
+    1
+  );
+  
 }
 
 void loop() {
-  checkTareButton();
+  vTaskDelay(pdMS_TO_TICKS(50));
+  
+  // checkTareButton();
 
-  switch (currentState) {
-    case IDLE:
-      scale.power_up();
-      berat = getWeight();
-      kategori = tentukanKategori(berat);
+  // switch (currentState) {
+  //   case IDLE:
+  //     scale.power_up();
+  //     berat = getWeight();
+  //     kategori = tentukanKategori(berat);
 
-      if (kategori != "stay") {
-        Serial.println("Berat: " + String(berat) + " Kg, Kategori: " + kategori);
+  //     if (kategori != "stay") {
+  //       // Serial.println("Berat: " + String(berat) + " Kg, Kategori: " + kategori);
+  //       Sheet.sendData(String(berat) + "Kg", kategori);
+
+  //       if (Sheet.getHttpCode() > 0) {
+  //         scale.power_down();
+  //         currentState = MOVING;
+  //       }
+  //     }
+  //     break;
+
+  //   case MOVING:
+  //     if (kategori == "GradeC") {
+  //       executeGradeC();
+  //     } else if (kategori == "GradeB") {
+  //       executeGradeB();
+  //     } else if (kategori == "GradeA") {
+  //       executeGradeA();
+  //     }
+  //     currentState = IDLE;
+  //     // Serial.println("loadcell ready");
+  //     break;
+
+  //   default:
+  //     currentState = IDLE;
+  //     break;
+  // }
+
+  // delay(50);
+}
+
+void applicationTask0(void *param){
+  Sheet.connectWiFi(ssid, password);
+  while (true)
+  {
+    // if (kategori != "stay") {
+      if (isServoMoving == true) {
         Sheet.sendData(String(berat) + "Kg", kategori);
-
-        if (Sheet.getHttpCode() > 0) {
-          scale.power_down();
-          currentState = MOVING;
-        }
+        // if (Sheet.getHttpCode() > 0) {
+        //   scale.power_down();
+        // }
+        isServoMoving = false;
       }
-      break;
-
-    case MOVING:
-      if (kategori == "GradeC") {
-        executeGradeC();
-      } else if (kategori == "GradeB") {
-        executeGradeB();
-      } else if (kategori == "GradeA") {
-        executeGradeA();
-      }
-      currentState = IDLE;
-      Serial.println("loadcell ready");
-      break;
-
-    default:
-      currentState = IDLE;
-      break;
+    // }
+    delay(50);
   }
+}
 
-  delay(50);
+void applicationTask1(void *param){
+  enum State {
+    IDLE,
+    MEASURING,
+    MOVING
+  };
+  State currentState = IDLE;
+  tareScale();
+  while (true)
+  {
+    checkTareButton();
+
+    switch (currentState) {
+      case IDLE:
+        scale.power_up();
+        berat = getWeight();
+        kategori = tentukanKategori(berat);
+  
+        if (kategori != "stay") {
+          isServoMoving = true;
+          currentState = MOVING;
+        }else{
+          isServoMoving = false;
+        }
+        break;
+  
+      case MOVING:
+        scale.power_down();
+        if (kategori == "GradeC") {
+          executeGradeC();
+        } else if (kategori == "GradeB") {
+          executeGradeB();
+        } else if (kategori == "GradeA") {
+          executeGradeA();
+        }
+        currentState = IDLE;
+        // Serial.println("loadcell ready");
+        break;
+  
+      default:
+        currentState = IDLE;
+        break;
+    }
+    delay(50);
+  }
 }
